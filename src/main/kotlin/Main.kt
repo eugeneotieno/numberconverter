77cffa040d672160e82165a8faa8c3913ed13b60
Eugene Otieno <otienoeo@gmail.com>
2023-01-03 16:18:59 +0300

    Convert to anu base.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,58 @@
+import java.math.BigInteger
+
 fun main() {
+    start()
+}
+
+fun start() {
     var exit = false
     do {
-        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ")
-        when(readln()) {
-            "/from" -> {
-                print("Enter a number in decimal system: ")
-                val num = readln().toInt()
-                print("Enter the target base: ")
-                val target = readln().toInt()
-                convertFromDecimal(num, target)
-            }
-            "/to" -> {
-                print("Enter source number: ")
+        print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ")
+        val action = readln()
+        if (action == "/exit") {
+            exit = true
+        } else {
+            val source = action.split(" ")[0]
+            val target = action.split(" ")[1]
+
+            var sameSource = true
+            while (sameSource) {
+                print("Enter number in base $source to convert to base $target (To go back type /back) ")
                 val num = readln()
-                print("Enter source base: ")
-                val target = readln().toInt()
-                convertToDecimal(num, target)
-            }
-            "/exit" -> {
-                exit = true
+
+                if (num == "/back") {
+                    sameSource = false
+                    println()
+                } else {
+                    if (source == "10") {
+                        convertFromDecimal(num.toBigInteger(), target)
+                    } else if (target == "10") {
+                        convertToDecimal(num, source.toInt())
+                    } else {
+                        val numDecimal = num.toBigInteger(source.toInt())
+                        val rs = numDecimal.toString(target.toInt())
+                        println("Conversion result: $rs")
+                        println()
+                    }
+                }
             }
         }
     } while (!exit)
 }
 
-fun convertFromDecimal(num: Int, target: Int) {
-    var result = ""
-    when(target) {
-        2 -> {
-            result = Integer.toBinaryString(num)
-        }
-        8 -> {
-            result = Integer.toOctalString(num)
-        }
-        16 -> {
-            result = Integer.toHexString(num)
-        }
-    }
-
-    println("Conversion result: $result")
+fun convertToDecimal(num: String, target: Int) {
+    println("Conversion result: ${num.toInt(radix = target)}")
     println()
 }
 
-fun convertToDecimal(num: String, target: Int) {
-    println("Conversion to decimal result: ${num.toInt(radix = target)}")
+fun convertFromDecimal(num: BigInteger, target: String) {
+    var holder = ""
+    var n = num
+    while (n > BigInteger.ZERO) {
+        holder += "${n % BigInteger.valueOf(target.toLong())}"
+        n /= BigInteger.valueOf(target.toLong())
+    }
+
+    println("Conversion result: ${holder.reversed()}")
     println()
 }
